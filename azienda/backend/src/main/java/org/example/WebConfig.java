b39a922adfc80438a893be529cabb3472ae9688b
Alien <eliamorari07@gmail.com>
2025-02-25 16:02:06 +0100

    added JDBC

--- azienda/backend/src/main/java/org/example/WebConfig.java
+++ azienda/backend/src/main/java/org/example/WebConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.sql.SQLException;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         // Aggiungi configurazione CORS globale per tutti gli endpoint
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")  // Permetti richieste solo da questo dominio
