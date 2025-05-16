299a45a885466db2717f4a5c5f0cbc989410e061
Alien <eliamorari07@gmail.com>
2025-05-16 12:17:26 +0200

    Added requests to api for saving datas

--- edificio/backend/src/main/java/org/example/Main.java
+++ edificio/backend/src/main/java/org/example/Main.java
@@ -1,21 +1,28 @@
 package org.example;
 
 import com.fazecast.jSerialComm.*;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         String portPath = "";
 
         for (int i = 0; i < SerialPort.getCommPorts().length; i++) {
+            System.out.println(SerialPort.getCommPorts()[i].getSystemPortName());
             if(SerialPort.getCommPorts()[i].getDescriptivePortName().startsWith("Arduino Uno")) {
                 portPath = SerialPort.getCommPorts()[i].getSystemPortName();
             } else if (SerialPort.getCommPorts()[i].getDescriptivePortName().startsWith("Serial Device")) {
                 portPath = "/dev/" + SerialPort.getCommPorts()[i].getSystemPortName();
             }
         }
+        if (portPath.isEmpty()) {
+            System.out.println("Errore nel trovare la giusta porta di arduino!");
+            return;
+        }
 
+        System.out.println("PortPath Selezionata: " + portPath);
         SerialPort comPort = SerialPort.getCommPort(portPath);
         comPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
@@ -26,22 +33,49 @@ public class Main {
         comPort.openPort();
         Thread.sleep(3000);
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Inserisci il comando: ");
-            String str = scanner.nextLine();
+        Thread threadMisure = new Thread(){
+            @Override
+            public void run() {
+                String[] commands = {"getTemperature","getHumidity","getCo2"};
 
-            byte[] sendData = (str+"\n").getBytes();
-            comPort.writeBytes(sendData, sendData.length);
-            System.out.println("MANDATO!");
-            while (true) {
-                if (arduinoSerialPortDataListener.isResFinished()) {
-                    System.out.println(arduinoSerialPortDataListener.getRes());
-                    break;
+                while (true) {
+                    for (int i = 0; i < commands.length; i++) {
+                        byte[] sendData = (commands[i] + "\n").getBytes();
+                        comPort.writeBytes(sendData, sendData.length);
+                        while (true) {
+                            if (arduinoSerialPortDataListener.isResFinished()) {
+                                store(commands[i]+";"+arduinoSerialPortDataListener.getRes());
+                                break;
+                            }
+                        }
+
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {break;}
                 }
+
             }
+        };
+        threadMisure.start();
+
+
+
+    }
+
+    public static void store(String str) {
+
+        System.out.println(str.split(";")[0] +" , " + str.split(";")[1]);
+        if (true) { //se già registrato
+            HttpResponse<String> response = Unirest.post("http://10.1.6.27:7070/api/arduino/add")
+                    .field(str.split(";")[0], str.split(";")[1])
+                    .asString();
+
+            System.out.println("Status: " + response.getStatus());
+            System.out.println("Body:\n" + response.getBody());
+
+        } else {
+
         }
-
-
     }
 }
