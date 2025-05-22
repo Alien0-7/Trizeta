67ea3f2e0748bc9020698d4571e99db7c59bc562
Fra-Miche <framiche001@gmail.com>
2025-05-22 16:29:55 +0200

    logout

--- azienda/frontend/src/pages/Settings.jsx
+++ azienda/frontend/src/pages/Settings.jsx
@@ -1,22 +1,24 @@
-import '../styles/Settings.css'
+import '../styles/Settings.css';
 import useSignOut from 'react-auth-kit/hooks/useSignOut';
-import '../styles/Profile.css'
+import '../styles/Profile.css';
 import { useNavigate } from 'react-router-dom';
 
-
-export default function Settings(){
+export default function Settings() {
 
     const signOut = useSignOut();
     const navigate = useNavigate();
 
-    const logout = () =>{
-        signOut();
-        navigate("/")
+    const logout = () => {
+        const confirmed = window.confirm("Sei sicuro di voler effettuare il logout?");
+        if (confirmed) {
+            signOut();
+            navigate("/");
+        }
     }
-    
-    return(
+
+    return (
         <>
-        <div className="LOGOUT" onClick={logout}>LOGOUT</div>
+            <div className="LOGOUT" onClick={logout}>LOGOUT</div>
         </>
     )
 }
