18cb217098c8a5580a7e85d2b9c3b67df1ed57b2
Alien <129426490+Alien0-7@users.noreply.github.com>
2025-05-23 12:08:38 +0200

    Added multi-threading library

--- edificio/arduino/sketch.ino
+++ edificio/arduino/sketch.ino
@@ -1,7 +1,8 @@
 #include <LiquidCrystal_I2C.h>
-#include  <Wire.h>
+#include <Wire.h>
 #include <DHT.h>
 #include "DHT.h"
+#include <TaskScheduler.h>
 
 #define DHTTYPE DHT11
 #define TRIG_PIN 10
@@ -12,31 +13,115 @@
 
 int StatoFinestra = digitalRead(Finestra);
 int analogPin = A0;
-int Co2 = 0; 
-int t = 0;
-int h = 0;
+float Co2 = 0;
+float t = 0;
+float h = 0;
 String inputString = "";
 boolean stringComplete = false;
 String UUID = "";
+Scheduler runner;
 
 DHT dht(SENSOR_PIN, DHTTYPE);
-LiquidCrystal_I2C lcd(0x27,  16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup() {
-  
-  lcd.init();
-  dht.begin();
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+void lcdVisualizer() {
+  StatoFinestra = digitalRead(Finestra);
+  /* questi "Serial.println" danno fastidio al programma quando cerca di ottenere le varie misurazioni
+  if (StatoFinestra == LOW) {
+    Serial.println("Finestra CHIUSA");
+  } else if (StatoFinestra == HIGH) {
+    Serial.println("Finestra APERTA");
+  }
+  */
+
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+  Co2 = analogRead(analogPin);
+
+  /* questi "Serial.println" danno fastidio al programma quando cerca di ottenere le varie misurazioni
+  Serial.println(t);
+  Serial.println(h);
+  Serial.println(Co2);
+  */
+
+  // Regolazione sensore ultrasuoni
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-  pinMode(AREAZIONE,OUTPUT);
-  Serial.begin(9600);
-  
-  
+
+  unsigned long tempo = pulseIn(ECHO_PIN, HIGH);
+  float distanza = 0.03438 * tempo / 2;
+
+  if (distanza <= 70) {
+
+    if (Co2 >= 93) {
+      lcd.backlight();
+      lcd.setCursor(0, 0);        // Impostiamo la prima riga
+      lcd.print("QUALITA'ARIA ");  // Scriviamo sulla prima riga
+      lcd.print(Co2);
+      lcd.setCursor(0, 1);        // Impostiamo la seconda riga
+      lcd.print("OTTIMA");       // Scriviamo sulla seconda riga
+      delay(2500);
+      lcd.clear();
+      lcd.setCursor(0, 0);  // Impostiamo la prima riga
+      lcd.print("TEMP");
+      lcd.setCursor(0, 1);  // Impostiamo la prima riga
+      lcd.print(t);
+      lcd.setCursor(6, 0);  // Impostiamo la prima riga
+      lcd.print("UMIDITA'");
+      lcd.setCursor(6, 1);  // Impostiamo la prima riga
+      lcd.print(h);
+      lcd.setCursor(8, 1);  // Impostiamo la prima riga
+      lcd.print("%");
+      delay(2500);
+      lcd.clear();
+    } else if (Co2 <= 92) {
+
+      lcd.backlight();
+      lcd.setCursor(0, 0);        // Impostiamo la prima riga
+      lcd.print("QUALITA'ARIA");  // Scriviamo sulla prima riga
+      lcd.setCursor(0, 1);        // Impostiamo la seconda riga
+      lcd.print("PESSIMA ");      // Scriviamo sulla seconda riga
+      delay(2500);
+      lcd.clear();
+      lcd.setCursor(0, 0);  // Impostiamo la prima riga
+      lcd.print("TEMP");
+      lcd.setCursor(0, 1);  // Impostiamo la prima riga
+      lcd.print(t);
+      lcd.setCursor(6, 0);  // Impostiamo la prima riga
+      lcd.print("UMIDITA'");
+      lcd.setCursor(6, 1);  // Impostiamo la prima riga
+      lcd.print(h);
+      lcd.setCursor(8, 1);  // Impostiamo la prima riga
+      lcd.print("%");
+      delay(2500);
+      lcd.clear();
+
+    }
+
+    else {
+      delay(5000);
+    }
+
+  } else {
+    lcd.clear();
+    lcd.noBacklight();
+    delay(5000);
+  }
+
+
+  // Per letture in background
+  if (Co2 >= 93) {
+    digitalWrite(AREAZIONE, LOW);
+  } else if (Co2 <= 92) {
+    digitalWrite(AREAZIONE, HIGH);
+  }
+
+  delay(100);
 }
 
 void listener() {
-    while (Serial.available()) {
+  while (Serial.available()) {
     char inChar = (char)Serial.read();
 
     if (inChar == '\n') {
@@ -47,7 +132,6 @@ void listener() {
     } else {
       inputString += inChar;
     }
-    
   }
   if (stringComplete) {
     if (inputString.equals("getTemperature")) {
@@ -69,101 +153,28 @@ void listener() {
   }
 }
 
-void loop() {
-  listener();
-  /*
-  StatoFinestra=digitalRead(Finestra);
+Task task1(1000, TASK_FOREVER, &listener);
+Task task2(1000, TASK_FOREVER, &lcdVisualizer);
 
-  if(StatoFinestra == LOW){
-    Serial.println("Finestra CHIUSA");
-  }
-  else if (StatoFinestra == HIGH){
-    Serial.println("Finestra APERTA");
-  }
 
-  t = dht.readTemperature();
-  h = dht.readHumidity();
-  Serial.println(t);
-  Serial.println(h);
-  Serial.println(Co2);
+void setup() {
+  runner.init();
+  runner.addTask(task1);
+  task1.enable();
 
-  Co2 = analogRead(analogPin);
-  
-  // Regolazione sensore ultrasuoni
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
+  runner.addTask(task2);
+  task2.enable();
+
+  lcd.init();
+  dht.begin();
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   digitalWrite(TRIG_PIN, LOW);
-
-  unsigned long tempo = pulseIn(ECHO_PIN, HIGH);
-  float distanza = 0.03438 * tempo / 2;
-
-  if (distanza <= 70) {
-
-  if (Co2 < 750) {
-    lcd.backlight();
-    lcd.setCursor(0, 0); // Impostiamo la prima riga
-    lcd.print("QUALITA'ARIA"); // Scriviamo sulla prima riga
-    lcd.setCursor(0, 1); // Impostiamo la seconda riga
-    lcd.print("OTTIMA "); // Scriviamo sulla seconda riga
-    delay(2500);
-    lcd.clear();
-    lcd.setCursor(0, 0); // Impostiamo la prima riga
-    lcd.print("TEMP");
-    lcd.setCursor(0, 1); // Impostiamo la prima riga
-    lcd.print(t);
-    lcd.setCursor(6, 0); // Impostiamo la prima riga
-    lcd.print("UMIDITA'");
-    lcd.setCursor(6, 1); // Impostiamo la prima riga
-    lcd.print(h);
-    lcd.setCursor(8, 1); // Impostiamo la prima riga
-    lcd.print("%");
-    delay(2500);
-    lcd.clear();
-  }
-  else if (Co2 > 750 ) {
-
-    lcd.backlight();
-    lcd.setCursor(0, 0); // Impostiamo la prima riga
-    lcd.print("QUALITA'ARIA"); // Scriviamo sulla prima riga
-    lcd.setCursor(0, 1); // Impostiamo la seconda riga
-    lcd.print("PESSIMA "); // Scriviamo sulla seconda riga
-    delay(2500);
-    lcd.clear();
-    lcd.setCursor(0, 0); // Impostiamo la prima riga
-    lcd.print("TEMP");
-    lcd.setCursor(0, 1); // Impostiamo la prima riga
-    lcd.print(t);
-    lcd.setCursor(6, 0); // Impostiamo la prima riga
-    lcd.print("UMIDITA'");
-    lcd.setCursor(6, 1); // Impostiamo la prima riga
-    lcd.print(h);
-    lcd.setCursor(8, 1); // Impostiamo la prima riga
-    lcd.print("%");
-    delay(2500);
-    lcd.clear();
-
-  }
-  
-  else {
-    delay(5000);
-  }
-*/
-}  
-else {
-  lcd.clear();
-  lcd.noBacklight();
-  delay(5000);
+  pinMode(AREAZIONE, OUTPUT);
+  Serial.begin(9600);
 }
 
 
-// Per letture in background
-if (Co2 < 750) {
-  digitalWrite(AREAZIONE, LOW);
-}
-else if (Co2 > 750) {
-  digitalWrite(AREAZIONE, HIGH);
-}
-
-delay(100);
-
+void loop() {
+  runner.execute();
 }
