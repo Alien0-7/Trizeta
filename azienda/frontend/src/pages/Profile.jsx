fdc1ab50b50932f1446dcd62b00e37bfa4f4e6ed
Fra-Miche <framiche001@gmail.com>
2025-05-24 11:11:42 +0200

    Added startingghraps

--- azienda/frontend/src/pages/Profile.jsx
+++ azienda/frontend/src/pages/Profile.jsx
@@ -1,11 +1,18 @@
 import '../styles/Profile.css'
+import {Chart as ChartJS} from "chart.js";
+import {Bar,Doughnut,Line} from "react-chartjs-2";
+
+
 
 export default function Profile() {
 
   return (
     <>
-      <div className='container'>
-        
+      <div className='body-container'>
+        <div id='temperature'>Temperature</div>
+        <div id='humidity'>Humidity</div>
+        <div id='airquality'>Air quality</div>
+        <div id='co2'>Co2</div>
       </div>
     </>
   );
